751fff3816219fd3d4947bc1739f4aef39f1ceda
skarasalamicrosoft <113906318+skarasalamicrosoft@users.noreply.github.com>
2023-09-15 11:17:04 +0530

    Update chat_with_website.py

--- chat_with_website.py
+++ chat_with_website.py
@@ -1,4 +1,4 @@
-﻿
+
 #!/usr/bin/env python
 # coding: utf-8
 
@@ -56,14 +56,36 @@ def main():
     # Set the title and subtitle of the app
     st.title('🦜🔗 Chat With Website')
     st.subheader('Input your website URL, ask questions, and receive answers directly from the website.')
-
+    button_style = """
+    position: absolute;
+    top: 20px;
+    right: 20px;
+    background-color: #f0f0f0;
+    padding: 10px;
+    border: none;
+    cursor: pointer;
+    border-radius: 5px;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+    """
+    st.markdown(
+    f"""
+    <style>
+        .dropdown-button {{
+            {button_style}
+        }}
+    </style>
+    """,
+    unsafe_allow_html=True,
+    )
+    Language = st.selectbox("Select Language", ["English", "Telugu", "French", "Spanish", "Hindi"])
+  
     url = st.text_input("Insert The website URL")
 
     prompt = st.text_input("Ask a question (query/prompt)")
     if st.button("Submit Query", type="primary"):
         response = openai.ChatCompletion.create(
                 engine="gpt-35-turbo-16k",
-                messages=[{ "role": "system", "content": url + "\n" + prompt }, { "role": "user", "content": "" }
+                messages=[{ "role": "system", "content": url + "\n" + prompt + " Give output in "+ Language}, { "role": "user", "content": "" }
                             ]
                 )
         st.write(response['choices'][0]['message']['content'])
